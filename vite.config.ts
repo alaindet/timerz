0ed7fbba04d221124055f54209bf72f063e1823f
Alain D'Ettorre <alain.det@gmail.com>
2025-11-01 01:18:25 +0100

    feat: add PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/timerz/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Service worker registration automatically
+      manifest: {
+        name: 'Timerz',
+        short_name: 'Timerz',
+        description: 'A simple app to create timers for a big display',
+        theme_color: '#242424',
+        background_color: '#242424',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    }),
+  ],
 })
